21effd2a83f5229739a0ee465218e99d1beb50fb
Nika <98172241+NINGWEII@users.noreply.github.com>
2022-01-30 17:49:30 +0400

    Add files via upload

---
+++ Question2.java
@@ -0,0 +1,18 @@
+
+public class Question2 {
+	public static void main(String[] args) {
+		
+		int arr[] = {5, 4, 6, 5 };
+		int sum = 0;
+		// Code below gives the sum of all elements in an array
+		for (int i = 0; i < arr.length; i++) {
+			 sum = sum + arr[i];
+		} System.out.println(sum); 
+		
+		// Code below gives the sum of last two elements in an array
+		for (int i = 0; i < arr.length - 1; i++) {
+			sum = arr[i] + arr[i + 1];
+		} System.out.println(sum);
+		
+	}
+}
