cd578eda27a36bbfc692c69d04826c8af9ab1daa
Nika <98172241+Ningweii@users.noreply.github.com>
2022-02-22 02:06:22 +0400

    Add files via upload

---
+++ ThisIsNotHappening.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ThisIsNotHappening {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	
+		
+
+		  
+			  
+    }
+}
+		 
